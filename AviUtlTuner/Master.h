647e205c798e26613643896837a19323627137b1
hebiiro <96464759+hebiiro@users.noreply.github.com>
2023-07-23 15:25:26 +0900

    :sparkles: コンフィグファイルでサーヴァントのON/OFFを切り替えられるようにしました

--- AviUtlTuner/Master.h
+++ AviUtlTuner/Master.h
@@ -10,20 +10,42 @@ namespace aut
 	{
 		std::unordered_map<std::wstring, Servant*> servants;
 
+		//
+		// 指定されたサーヴァントと契約します。
+		//
 		template<class T>
 		BOOL contract_with(T* servant)
 		{
 			servants[T::getServantNameStatic()] = servant;
-
 			return TRUE;
 		}
 
+		//
+		// 指定されたサーヴァントと契約します。
+		// ただし、コンフィグファイルで Enable になっていることが条件です。
+		//
+		template<class T>
+		BOOL contract_with(T* servant, LPCWSTR path)
+		{
+			BOOL enable = TRUE;
+			getPrivateProfileInt(path, L"Enable", T::getServantNameStatic(), enable);
+			if (!enable) return FALSE;
+			servants[T::getServantNameStatic()] = servant;
+			return TRUE;
+		}
+
+		//
+		// 指定された型のサーヴァントを召喚します。
+		//
 		template<class T>
 		T* summon() const
 		{
 			return summon<T>(T::getServantNameStatic());
 		}
 
+		//
+		// 指定された名前のサーヴァントを召喚します。
+		//
 		template<class T>
 		T* summon(LPCWSTR name) const
 		{
@@ -34,10 +56,26 @@ namespace aut
 			}
 		}
 
+		//
+		// サーヴァントとの契約を開始します。
+		// 内部的に使用されます。
+		//
+		BOOL contract()
+		{
+			std::wstring path = hive.getConfigFileName(L"AviUtlTuner.ini");
+
+			contract_with(&editbox_tweaker, path.c_str());
+			contract_with(&exedit_tweaker, path.c_str());
+
+			return TRUE;
+		}
+
+		//
+		// マスターの初期化を実行します。
+		//
 		BOOL init()
 		{
-			contract_with(&editbox_tweaker);
-			contract_with(&exedit_tweaker);
+			contract();
 
 			// サーヴァントに初期化を実行させます。
 			for (const auto& pair : servants)
@@ -46,9 +84,12 @@ namespace aut
 			return TRUE;
 		}
 
+		//
+		// マスターの後始末を実行します。
+		//
 		BOOL exit()
 		{
-			// サーヴァントに後始末を実行させます。(逆順で)
+			// サーヴァントに後始末を実行させます。
 			for (const auto& pair : servants)
 				pair.second->on_exit();
 
