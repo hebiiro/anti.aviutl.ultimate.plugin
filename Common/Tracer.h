2a94a33d73f669413233653a836f0cea286402ce
hebiiro <io.hebiiro@gmail.com>
2023-10-01 14:39:22 +0900

    :zap: 自動的に関数名を出力する機能を追加しました

--- Common/Tracer.h
+++ Common/Tracer.h
@@ -3,10 +3,12 @@
 
 #ifdef _DEBUG
 #define MY_TRACE(format, ...)		Tools::Tracer::output_format(_T(__FILE__), __LINE__, format, __VA_ARGS__)
+#define MY_TRACE_EX(format, ...)		Tools::Tracer::output_format(_T(__FILE__), __LINE__, _T(__FUNCSIG__), _T(format), __VA_ARGS__)
 #define MY_TRACE_BINARY(buf, c)		Tools::Tracer::output_binary(_T(__FILE__), __LINE__, buf, c)
 #define MY_TRACE_FUNC(format, ...)	Tools::Tracer::output_func(_T(__FILE__), __LINE__, _T(__FUNCSIG__), _T(format), __VA_ARGS__)
 #else
 #define MY_TRACE(format, ...)		((void)0)
+#define MY_TRACE_EX(format, ...)		((void)0)
 #define MY_TRACE_BINARY(buf, c)		((void)0)
 #define MY_TRACE_FUNC(format, ...)	((void)0)
 #endif
@@ -58,15 +60,85 @@ while (0)
 
 namespace Tools {
 	struct Tracer {
+		//
+		// 文字列クラスです。
+		//
 		using string = std::basic_string<TCHAR>;
 
+		//
+		// 最大文字数です。
+		//
 		inline static constexpr int maxSize = 2048;
+
+		//
+		// 文字列を出力するデフォルトのクラスです。
+		//
 		inline static struct Logger {
 			virtual void output(LPCTSTR raw, LPCTSTR text) {
 				::OutputDebugString(text);
 			}
 		} default_logger;
+
+		//
+		// 文字列を出力するクラスです。
+		//
 		inline static Logger* logger = &default_logger;
+
+		//
+		// origに含まれるold_stringをnew_stringに置換して返します。
+		//
+		inline static string replace(const string& orig, LPCTSTR old_string, LPCTSTR new_string) {
+			string result;
+			string::size_type old_string_length = _tcslen(old_string);
+			string::size_type begin_pos = 0;
+			while (1) {
+				// old_stringを検索します。
+				string::size_type end_pos = orig.find(old_string, begin_pos);
+				if (end_pos != string::npos) {
+					// old_stringが見つかった場合の処理です。
+					// begin_posからend_posまでを結果に追加します。
+					result.append(orig, begin_pos, end_pos - begin_pos);
+					// さらにnew_stringを結果に追加します。
+					result.append(new_string);
+					// 検索開始位置を進めます。
+					begin_pos = end_pos + old_string_length;
+				} else {
+					// old_stringが見つからなかった場合の処理です。
+					// begin_posから最後までを結果に追加します。
+					result.append(orig, begin_pos);
+					// ループを終了します。
+					break;
+				}
+			}
+			return result;
+		}
+
+		//
+		// __FUNCSIG__を整形して返します。
+		//
+		inline static string get_func_name(LPCTSTR func) {
+			string func_name = func;
+
+			// "unnamed-type-"を削除します。
+			func_name = replace(func_name, _T("unnamed-type-"), _T(""));
+
+			// 最初の'('を検索します。
+			size_t end = func_name.find_first_of(_T('('));
+
+			// 見つかった場合はリサイズして'('以降を削除します。
+			if (end != string::npos) func_name.resize(end);
+
+			// 最初の'<'を検索します。
+			size_t template_begin = func_name.find_first_of(_T('<'));
+
+			// 最初の'<'より前にある最後の' 'を検索します。
+			size_t begin = func_name.find_last_of(_T(' '), template_begin);
+
+			// 見つかった場合は先頭から' 'までを削除します。
+			if (begin != string::npos) func_name.erase(0, begin + 1);
+
+			return func_name;
+		}
 
 		//
 		// 文字列を修飾して出力します。
@@ -91,6 +163,27 @@ namespace Tools {
 		static void output_format(LPCTSTR file, int line, LPCTSTR format, Args... args) {
 			FormatString text(maxSize, format, args...);
 			output_text(file, line, text.c_str());
+		}
+
+		//
+		// 関数名付きで書式化された文字列を出力します。
+		//
+		template <class... Args>
+		static void output_format(LPCTSTR file, int line, LPCTSTR func, LPCTSTR format, Args... args) {
+			//
+			// funcからfunc_nameを構築します。
+			//
+			string func_name = get_func_name(func);
+
+			//
+			// formatとargsからtextを構築します。
+			//
+			FormatString text(maxSize, format, args...);
+
+			//
+			// func_nameとtextを書式化して出力します。
+			//
+			output_format(file, line, _T("%s : %s\n"), func_name, text);
 		}
 
 		static TCHAR ch(BYTE a) {
@@ -141,12 +234,7 @@ namespace Tools {
 			//
 			// funcからfunc_nameを構築します。
 			//
-			string func_name = func;
-			size_t end = func_name.find_first_of(_T('('));
-			if (end != string::npos) func_name.resize(end);
-			size_t template_begin = func_name.find_first_of(_T('<'));
-			size_t begin = func_name.find_last_of(_T(' '), template_begin);
-			if (begin != string::npos) func_name.erase(0, begin + 1);
+			string func_name = get_func_name(func);
 
 			//
 			// func_nameとfunc_argsを書式化して出力します。
