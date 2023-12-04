eb5ee61c3241ba71bc6e783533d1dffecd3c602e
hebiiro <io.hebiiro@gmail.com>
2023-12-04 21:32:17 +0900

    :bookmark: r9

--- Ultimate/Main.cpp
+++ Ultimate/Main.cpp
@@ -104,7 +104,7 @@ namespace fgo
 			.func_exit = func_exit,
 			.func_update = func_update,
 			.func_WndProc = func_WndProc,
-			.information = "アルティメットプラグイン r8",
+			.information = "アルティメットプラグイン r9",
 			.func_save_start = func_save_start,
 			.func_save_end = func_save_end,
 			.func_is_saveframe = func_is_saveframe,
