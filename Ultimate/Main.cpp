1fecfdb8bbc758d99e520e636060fa4fe7879e36
hebiiro <io.hebiiro@gmail.com>
2023-12-13 15:27:02 +0900

    :bookmark: r12

--- Ultimate/Main.cpp
+++ Ultimate/Main.cpp
@@ -104,7 +104,7 @@ namespace fgo
 			.func_exit = func_exit,
 			.func_update = func_update,
 			.func_WndProc = func_WndProc,
-			.information = "アルティメットプラグイン r11",
+			.information = "アルティメットプラグイン r12",
 			.func_save_start = func_save_start,
 			.func_save_end = func_save_end,
 			.func_is_saveframe = func_is_saveframe,
