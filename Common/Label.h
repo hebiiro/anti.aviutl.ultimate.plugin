837f0f8d5ed17602c34364ac41d77f86bc45d589
hebiiro <io.hebiiro@gmail.com>
2024-02-11 19:18:10 +0900

    ♻️ Labelをテンプレート化しました

--- Common/Label.h
+++ Common/Label.h
@@ -5,9 +5,15 @@ namespace Tools
 	//
 	// このクラスはラベルの値とテキストを保持します。
 	//
-	struct Label
+	template <class V = int, class T = LPCWSTR>
+	struct LabelT
 	{ 
-		int value;
-		LPCWSTR text;
+		V value;
+		T text;
 	};
+
+	//
+	// このクラスはラベルの実体です。
+	//
+	struct Label : LabelT<int> {};
 }
