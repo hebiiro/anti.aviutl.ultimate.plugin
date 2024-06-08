2f82dc3ee919c541546668e668f018ec3be66340
hebiiro <io.hebiiro@gmail.com>
2024-06-08 11:07:27 +0900

    🩹 エディットボックスの非クライアント領域がちらつく問題に対応

--- dark.aua/gdi/renderer_nc.hpp
+++ dark.aua/gdi/renderer_nc.hpp
@@ -13,9 +13,20 @@ namespace apn::dark::gdi
 					MY_TRACE_FUNC("WM_NCPAINT, {:#010x}, {:#010x}, {:#010x}",
 						current_state->hwnd, current_state->wParam, current_state->lParam);
 
-					__super::on_subclass_proc(current_state);
-
 					auto hwnd = current_state->hwnd;
+					auto style = my::get_style(hwnd);
+
+					// キャプションやスクロールバーが存在する場合は
+					if ((style & WS_CAPTION) == WS_CAPTION ||
+						(style & WS_HSCROLL) == WS_HSCROLL ||
+						(style & WS_VSCROLL) == WS_VSCROLL)
+					{
+						// 先にデフォルトの描画処理を実行します。
+						__super::on_subclass_proc(current_state);
+					}
+
+					// 非クライアント領域を描画します。
+
 					auto rc = my::get_window_rect(hwnd);
 					auto origin = POINT { rc.left, rc.top };
 					::OffsetRect(&rc, -origin.x, -origin.y);
