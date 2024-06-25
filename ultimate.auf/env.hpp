f04447230d9ea45f8b3c2fd24bde184298b8146b
hebiiro <io.hebiiro@gmail.com>
2024-06-25 11:23:04 +0900

    ♻️ pluginsフォルダに移動

--- ultimate.auf/env.hpp
+++ ultimate.auf/env.hpp
@@ -10,7 +10,16 @@ namespace apn
 
 			MY_TRACE_STR(hive.python_file_name);
 
-			::LoadLibraryEx(hive.python_file_name.c_str(), nullptr, LOAD_WITH_ALTERED_SEARCH_PATH);
+			std::filesystem::path file_name = hive.python_file_name;
+			if (file_name.is_relative())
+			{
+				auto module_file_name = my::get_module_file_name(hive.instance);
+				file_name = module_file_name.parent_path() / hive.python_file_name;
+			}
+			MY_TRACE_STR(file_name);
+
+			auto python32 = ::LoadLibraryEx(file_name.c_str(), nullptr, LOAD_WITH_ALTERED_SEARCH_PATH);
+			MY_TRACE_HEX(python32);
 
 			return TRUE;
 		}
