d1009d714200e35542113bbd390357bf4d49cf1d
hebiiro <io.hebiiro@gmail.com>
2025-01-17 09:13:41 +0900

    🩹タイトルバーがモニターの範囲外に出てしまう問題に対応

--- source/dialog_position.aua/dialog.hpp
+++ source/dialog_position.aua/dialog.hpp
@@ -5,7 +5,7 @@ namespace apn::dialog_position
 	//
 	// このクラスはダイアログをフックします。
 	//
-	struct Dialog : my::Window
+	struct Dialog : my::WindowInterseptor
 	{
 		//
 		// ウィンドウプロシージャです。
@@ -29,23 +29,19 @@ namespace apn::dialog_position
 
 					// ウィンドウ矩形がモニタの範囲外ならオフセットを算出します。
 					auto offset = POINT {};
-					if (rc.right > monitor.right) offset.x = monitor.right - rc.right;
-					if (rc.bottom > monitor.bottom) offset.y = monitor.bottom - rc.bottom;
 					if (rc.left < monitor.left) offset.x = monitor.left - rc.left;
 					if (rc.top < monitor.top) offset.y = monitor.top - rc.top;
+					if (my::get_width(rc) <= my::get_width(monitor))
+						if (rc.right > monitor.right) offset.x = monitor.right - rc.right;
+					if (my::get_height(rc) <= my::get_height(monitor))
+						if (rc.bottom > monitor.bottom) offset.y = monitor.bottom - rc.bottom;
 
+					// オフセットが有効の場合は
 					if (offset.x || offset.y)
 					{
 						// モニタ内に収まるようにウィンドウ矩形を調整します。
-
 						::OffsetRect(&rc, offset.x, offset.y);
-
-						auto x = rc.left;
-						auto y = rc.top;
-						auto w = my::get_width(rc);
-						auto h = my::get_height(rc);
-
-						::SetWindowPos(hwnd, nullptr, x, y, w, h, SWP_NOZORDER);
+						my::set_window_pos(hwnd, nullptr, &rc, SWP_NOZORDER);
 					}
 
 					return result;
