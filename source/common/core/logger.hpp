ea233808223ba062bee47ae6e80cb373dbffdf18
hebiiro <io.hebiiro@gmail.com>
2026-08-16 10:11:15 +0900

    ⚡️キー限定出力とファイル出力を両立できるように変更

--- source/common/core/logger.hpp
+++ source/common/core/logger.hpp
@@ -2,32 +2,54 @@
 
 namespace apn
 {
+	//
+	// このクラスは指定されたキーが押されているときだけログを出力するようにします。
+	//
+	template <int vk>
+	struct logger_width_key_t : my::Tracer::Logger
+	{
+		//
+		// オリジナルのロガーです。
+		//
+		my::Tracer::Logger* orig = {};
+
+		//
+		// コンストラクタです。
+		//
+		logger_width_key_t()
+		{
+			// オリジナルロガーを取得しておきます。
+			orig = my::Tracer::logger;
+
+			// このロガーをセットします。
+			my::Tracer::logger = this;
+		}
+
+		//
+		// この仮想関数をはログを出力するときに呼び出されます。
+		//
+		virtual void output(LPCTSTR raw, LPCTSTR text) override
+		{
+			// 指定されたキーが押されているときだけログを出力します。
+			if (::GetKeyState(vk) < 0) orig->output(raw, text);
+		}
+	};
+
 	inline void set_logger(LPCWSTR args)
 	{
 #if defined(_DEBUG) && 1
 		if (my::contains(args, L"f1"))
 		{
-			// カスタムロガーを設定します。
-			static struct Logger : my::Tracer::Logger {
-				virtual void output(LPCTSTR raw, LPCTSTR text) override {
-					// F1キーが押されているときだけログを出力します。
-					if (::GetKeyState(VK_F1) < 0) ::OutputDebugString(text);
-				}
-			} logger;
-			my::Tracer::logger = &logger;
+			// カスタムロガーをセットします。
+			static logger_width_key_t<VK_F1> f1_logger;
 		}
 		else if (my::contains(args, L"shift"))
 		{
-			// カスタムロガーを設定します。
-			static struct Logger : my::Tracer::Logger {
-				virtual void output(LPCTSTR raw, LPCTSTR text) override {
-					// Shiftキーが押されているときだけログを出力します。
-					if (::GetKeyState(VK_SHIFT) < 0) ::OutputDebugString(text);
-				}
-			} logger;
-			my::Tracer::logger = &logger;
+			// カスタムロガーをセットします。
+			static logger_width_key_t<VK_SHIFT> shift_logger;
 		}
 #endif
+		// デバッグが設定されていない場合はロガーをリセットします。
 		if (!my::contains(args, L"debug")) my::reset(my::Tracer::logger);
 	}
 }
